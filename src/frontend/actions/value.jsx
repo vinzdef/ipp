031641e1bbe4f1ba517aceaff098255c1ef85c7c
Vincent De Feo <ghzmdr@gmail.com>
2018-06-23 00:17:20 +0200

    [HOTFIX] Dynamic hosts in frontend

--- src/frontend/actions/value.jsx
+++ src/frontend/actions/value.jsx
@@ -3,7 +3,7 @@ import {UPDATE_VALUE, INCREASE_SUCCESS, INCREASE_FAILURE} from './types'
 export function subscribeValueUpdates() {
 	
 	return function(dispatch) {
-		const ws = new WebSocket('ws://localhost:3000/v1/websocket')
+		const ws = new WebSocket(`ws://${window.location.hostname}:3000/v1/websocket`)
 		ws.addEventListener('message', ({data}) => {
 			dispatch({type: UPDATE_VALUE, value: data})
 		})
@@ -12,7 +12,7 @@ export function subscribeValueUpdates() {
 
 export function increaseValue() {
 	return function (dispatch) {		
-		fetch('//localhost:3000/v1/increase', {mode: 'no-cors', method: 'POST'})
+		fetch(`//${window.location.hostname}:3000/v1/increase`, {mode: 'no-cors', method: 'POST'})
 			.then(dispatch({type: INCREASE_SUCCESS}))
 			.catch(dispatch({type: INCREASE_FAILURE}))
 	}
